92f5eb890a36eb4c5db7bb76c97881b93f8abfd0
Michael Bely <michael-bel@outlook.com>
2022-01-20 20:22:17 +0300

    Update project

---
+++ core/src/main/kotlin/org/michaelbel/core/ktx/Date.kt
@@ -0,0 +1,27 @@
+@file:Suppress("unused")
+
+package org.michaelbel.core.ktx
+
+import java.util.Calendar
+import java.util.Date
+
+var Date.calendar: Calendar
+    get() = Calendar.getInstance().apply { time = this@calendar }
+    set(value) {
+        time = value.timeInMillis
+    }
+
+val Date.isToday: Boolean
+    get() {
+        val calToday = Calendar.getInstance()
+        return calToday.get(Calendar.ERA) == calendar.get(Calendar.ERA) &&
+                calToday.get(Calendar.YEAR) == calendar.get(Calendar.YEAR) &&
+                calToday.get(Calendar.DAY_OF_YEAR) == calendar.get(Calendar.DAY_OF_YEAR)
+    }
+
+val Date.isTomorrow: Boolean
+    get() {
+        val tomorrow = Calendar.getInstance().apply { add(Calendar.DAY_OF_YEAR, 1) }
+        return tomorrow.get(Calendar.YEAR) == calendar.get(Calendar.YEAR) &&
+                tomorrow.get(Calendar.DAY_OF_YEAR) == calendar.get(Calendar.DAY_OF_YEAR)
+    }
