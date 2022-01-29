0329f56e5e3f0f094d0fa242f39034c607385784
Michael Bely <michael-bel@outlook.com>
2022-01-29 11:34:37 +0300

    Update Project

--- buildSrc/src/main/kotlin/Kotlin.kt
+++ buildSrc/src/main/kotlin/Kotlin.kt
@@ -6,37 +6,25 @@ package org.michaelbel.template
  * https://github.com/JetBrains/kotlin/releases
  * https://github.com/Kotlin/kotlinx.coroutines/releases
  * https://github.com/Kotlin/kotlinx.serialization/releases
+ * https://github.com/Kotlin/kotlinx-datetime/releases
  */
 object Kotlin {
-    private const val Version = "1.6.0"
-    private const val CoroutinesVersion = "1.6.0"
-    private const val SerializationVersion = "1.3.2"
+    private const val KotlinVersion = "1.6.0"
+    private const val KotlinCoroutinesVersion = "1.6.0"
+    private const val KotlinSerializationVersion = "1.3.2"
+    private const val KotlinDatetimeVersion = "0.3.2"
 
-    const val CoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$CoroutinesVersion"
-    const val CoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$CoroutinesVersion"
-    const val CoroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$CoroutinesVersion"
-    const val Serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:$SerializationVersion"
+    const val KotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KotlinVersion"
+    const val KotlinSerializationPlugin = "org.jetbrains.kotlin:kotlin-serialization:$KotlinVersion"
+
+    const val KotlinCoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$KotlinCoroutinesVersion"
+    const val KotlinCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$KotlinCoroutinesVersion"
+    const val KotlinCoroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$KotlinCoroutinesVersion"
+    const val KotlinSerialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:$KotlinSerializationVersion"
+    const val KotlinDatetime = "org.jetbrains.kotlinx:kotlinx-datetime:$KotlinDatetimeVersion"
 
     object Options {
-        private const val ExperimentalCoroutinesApi = "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
-        private const val ObsoleteCoroutinesApi = "-Xuse-experimental=kotlinx.coroutines.ObsoleteCoroutinesApi"
-        private const val ExperimentalPagingApi = "-Xuse-experimental=androidx.paging.ExperimentalPagingApi"
-        private const val ExperimentalComposeUiApi = "-Xuse-experimental=androidx.compose.ui.ExperimentalComposeUiApi"
-        private const val ExperimentalMaterialApi = "-Xuse-experimental=androidx.compose.material.ExperimentalMaterialApi"
-        private const val ExperimentalMaterial3Api = "-Xuse-experimental=androidx.compose.material3.ExperimentalMaterial3Api"
-        private const val ExperimentalCoilApi = "-Xuse-experimental=coil.annotation.ExperimentalCoilApi"
-        private const val ExperimentalSerializationApi = "-Xuse-experimental=kotlinx.serialization.ExperimentalSerializationApi"
-        private const val FlowPreview = "-Xopt-in=kotlinx.coroutines.FlowPreview"
-
-        val all: String
-            get() = ExperimentalCoroutinesApi +
-                    ObsoleteCoroutinesApi +
-                    ExperimentalPagingApi +
-                    ExperimentalComposeUiApi +
-                    ExperimentalMaterialApi +
-                    ExperimentalMaterial3Api +
-                    ExperimentalCoilApi +
-                    ExperimentalSerializationApi +
-                    FlowPreview
+        const val OptExperimentalMaterial3Api = "-Xopt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        const val OptExperimentalFoundationApi = "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi"
     }
 }
