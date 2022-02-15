e32a666109113cd6790d398ead05434f556da8dd
michaelbel <michael-bel@outlook.com>
2022-02-15 22:29:12 +0300

    Update Project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,19 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.namespace) {
+                "com.android" -> {
+                    useModule("com.android.tools.build:gradle:${requested.version}")
+                }
+            }
+        }
+    }
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
 include(":app", ":core")
 rootProject.name = "android-app-template"
