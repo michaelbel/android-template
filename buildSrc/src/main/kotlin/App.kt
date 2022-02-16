21fc864e4788255f24023d5f72e1894550674c74
michaelbel <michael-bel@outlook.com>
2022-02-16 22:12:59 +0300

    Update Project

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -4,8 +4,8 @@ object App {
     const val ApplicationId = "org.michaelbel.template"
     const val VersionName = "1.0.0"
     const val MinSdk = 21
-    const val TargetSdk = 31
-    const val CompileSdk = 31
-    const val BuildTools = "31.0.0"
+    const val TargetSdk = 32
+    const val CompileSdk = 32
+    const val BuildTools = "32.0.0"
     const val GradlePlugin = "com.android.tools.build:gradle:7.1.1"
 }
