1c0aa1ff842dd65afe5860b9e1e8443ddaa9766b
michaelbel <michael-bel@outlook.com>
2022-03-23 13:19:20 +0300

    Update Project

--- core/src/main/kotlin/org/michaelbel/core/ktx/Bundle.kt
+++ core/src/main/kotlin/org/michaelbel/core/ktx/Bundle.kt
@@ -2,25 +2,25 @@
 
 package org.michaelbel.core.ktx
 
+import android.app.Activity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import kotlin.reflect.KProperty
 
-/**
- * Function that simplify android.os.Bundle unpacking.
- * @param provideArguments block function, that expect android.os.Bundle from object
- *
- * Usage example:
- * private val wrapper = { f: Fragment -> a.arguments }
- *
- * @return ReadOnlyProperty, Usage example:
- * val argument: Int by argumentsDelegate(wrapper)
- */
+inline fun <reified T> Activity.argumentDelegate(): LazyProvider<Activity, T> {
+    return argumentDelegate { activity: Activity -> activity.intent?.extras }
+}
+
+inline fun <reified T> Fragment.argumentDelegate(): LazyProvider<Fragment, T> {
+    return argumentDelegate { fragment: Fragment -> fragment.requireArguments() }
+}
+
 inline fun <F, reified T> argumentDelegate(
     crossinline provideArguments: (F) -> Bundle?
 ): LazyProvider<F, T> =
         object: LazyProvider<F, T> {
             override fun provideDelegate(thisRef: F, prop: KProperty<*>): Lazy<T> = lazy {
-                val bundle = provideArguments(thisRef)
+                val bundle: Bundle? = provideArguments(thisRef)
                 bundle?.get(prop.name) as T
             }
         }
