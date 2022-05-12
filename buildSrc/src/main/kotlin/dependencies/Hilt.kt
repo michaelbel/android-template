a6f2bd4c1498070e56759eb962acd65bfec0db41
michaelbel <michael-bel@outlook.com>
2022-05-12 13:11:45 +0300

    Update Project

--- buildSrc/src/main/kotlin/dependencies/Hilt.kt
+++ buildSrc/src/main/kotlin/dependencies/Hilt.kt
@@ -12,15 +12,13 @@ import org.michaelbel.template.extensions.kapt
  * @see <a href="https://github.com/google/dagger/releases">Hilt</a>
  */
 
-private const val HiltVersion = "2.41"
+private const val HiltVersion = "2.42"
 private const val HiltViewModelVersion = "1.0.0-alpha03"
 
 const val HiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:$HiltVersion"
 private const val HiltAndroid = "com.google.dagger:hilt-android:$HiltVersion"
 private const val HiltCompiler = "com.google.dagger:hilt-compiler:$HiltVersion"
 private const val HiltViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:$HiltViewModelVersion"
-
-const val HiltAndroidPlugin = "dagger.hilt.android.plugin"
 
 fun DependencyHandler.implementationHiltDependencies() {
     implementation(HiltAndroid)
