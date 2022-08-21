8ffdc19185437e51f0c3d1dbd98f6f93ccc10ff4
michaelbel <michael-bel@outlook.com>
2022-08-21 20:18:26 +0300

    Update project

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -1,6 +1,6 @@
+@file:Suppress("UnstableApiUsage")
+
 import org.michaelbel.template.CompileSdk
-import org.michaelbel.template.MinSdk
-import org.michaelbel.template.TargetSdk
 import org.michaelbel.template.dependencies.KotlinCompilerExtensionVersion
 import org.michaelbel.template.dependencies.implementationFacebookDependencies
 import org.michaelbel.template.dependencies.implementationHiltDependencies
@@ -16,21 +16,6 @@ plugins {
 
 android {
     compileSdk = CompileSdk
-
-    defaultConfig {
-        minSdk = MinSdk
-        targetSdk = TargetSdk
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
 
     composeOptions {
         kotlinCompilerExtensionVersion = KotlinCompilerExtensionVersion
