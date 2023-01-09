b79f008274f48820ef27bf146524d6fb00f85389
michaelbel <michael-bel@outlook.com>
2023-01-09 17:23:39 +0400

    Update project

--- core/src/main/kotlin/org/michaelbel/core/ktx/Date.kt
+++ core/src/main/kotlin/org/michaelbel/core/ktx/Date.kt
@@ -2,9 +2,13 @@
 
 package org.michaelbel.core.ktx
 
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
+
+private const val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
 
 var Date.calendar: Calendar
     get() = Calendar.getInstance().apply { time = this@calendar }
@@ -30,3 +34,12 @@ val Date.isTomorrow: Boolean
 fun diffBetweenDates(date1: Date, date2: Date): Long {
     return TimeUnit.MILLISECONDS.toMinutes(date1.time - date2.time)
 }
+
+val String.datetimeToCalendar: Calendar
+    get() {
+        val simpleDateFormat = SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault())
+        val date: Date = simpleDateFormat.parse(this) ?: return Calendar.getInstance()
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        return calendar
+    }
