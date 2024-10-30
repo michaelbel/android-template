7cd26cdec0f874b35ee21e4bb73f1afaff19e7fb
michaelbel <michael-bel@outlook.com>
2024-10-30 21:56:39 +0300

    Update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.androidx.navigation.safeargs) apply false
 }
