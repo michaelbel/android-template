6284fdde71d8fdc6cec79cc5c6f7ca1010dbaab3
michaelbel <michael-bel@outlook.com>
2025-01-06 06:21:17 +0530

    Update project

--- wear/src/main/kotlin/org/michaelbel/template/ui/AppNavigation.kt
+++ wear/src/main/kotlin/org/michaelbel/template/ui/AppNavigation.kt
@@ -3,7 +3,6 @@ package org.michaelbel.template.ui
 sealed class AppNavigation(
     val route: String
 ) {
-
     data object List: AppNavigation("list")
 
     data object Details: AppNavigation("details")
