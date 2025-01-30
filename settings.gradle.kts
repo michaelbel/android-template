73cfe76efb28a24a22fd99f7c64dc5d510af553d
michaelbel <michael-bel@outlook.com>
2025-01-30 12:26:52 +0300

    Update project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,27 @@
 
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "android-template" // todo Replace with your own app’s name
 include(
     ":mobile",
     ":wear",
@@ -21,6 +31,3 @@ include(
     ":xr",
     ":core"
 )
-rootProject.name = "android-template" // todo Replace with your own app’s name
-
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
