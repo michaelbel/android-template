43ed1aa94507c8a5cf1fbbf3f0ab4b847d97562f
michaelbel <michael-bel@outlook.com>
2025-02-05 19:33:41 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/MainActivity.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/MainActivity.kt
@@ -1,14 +1,14 @@
 package org.michaelbel.template
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.michaelbel.template.ui.AppTheme
 import org.michaelbel.template.ui.MainActivityContent
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
