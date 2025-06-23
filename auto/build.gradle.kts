829abc454fc0b20eaefbbd95a3a3f5fa2f5600b9
michaelbel <michael-bel@outlook.com>
2025-06-23 10:32:35 +0300

    Update project

--- auto/build.gradle.kts
+++ auto/build.gradle.kts
@@ -97,7 +97,7 @@ android {
 }
 
 base {
-    archivesName.set("Auto-Template-v${android.defaultConfig.versionName}(${android.defaultConfig.versionCode})") // Replace with your own app's name
+    archivesName.set("Auto-Template-v${android.defaultConfig.versionName}(${android.defaultConfig.versionCode})") // fixme Replace with your own app's name
 }
 
 dependencies {
