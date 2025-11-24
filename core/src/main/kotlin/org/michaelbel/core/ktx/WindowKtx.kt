db84494c4545e9b4ccb30d5df0c33c945e187950
michaelbel <michael-bel@outlook.com>
2025-11-24 19:46:25 -0300

    Update project

--- core/src/main/kotlin/org/michaelbel/core/ktx/WindowKtx.kt
+++ core/src/main/kotlin/org/michaelbel/core/ktx/WindowKtx.kt
@@ -3,9 +3,16 @@
 package org.michaelbel.core.ktx
 
 import android.os.Build
+import android.provider.Settings
 import android.view.Window
 import android.view.WindowManager.LayoutParams
 import androidx.annotation.ColorRes
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemGestures
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
@@ -38,4 +45,16 @@ fun Window.updateAttributes(block: LayoutParams.() -> Unit) {
     layoutParams.copyFrom(attributes)
     layoutParams.apply(block)
     attributes = layoutParams
+}
+
+@Composable
+fun isThreeButtonNav(): Boolean {
+    val context = LocalContext.current
+    val v = Settings.Secure.getInt(context.contentResolver, "navigation_mode", -1)
+    if (v in 0..2) return v == 0
+    val density = LocalDensity.current
+    val layoutDirection = LocalLayoutDirection.current
+    val gestures = WindowInsets.systemGestures
+    val hasGestureInsets = gestures.getLeft(density, layoutDirection) > 0 || gestures.getRight(density, layoutDirection) > 0 || gestures.getTop(density) > 0 || gestures.getBottom(density) > 0
+    return !hasGestureInsets
 }
