4f76e8b6e2e571e0701377a2261bf28e94e7d698
michaelbel <michael-bel@outlook.com>
2026-01-23 17:46:12 +0300

    Update project

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.google.ksp)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -5,7 +5,6 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.ksp)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.compose)
 }
