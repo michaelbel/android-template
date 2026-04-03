fdff80f063b0a41bad6a3f12731308d88df1d952
michaelbel <michael-bel@outlook.com>
2026-04-03 18:41:10 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/compact/details/DetailsScreen.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/compact/details/DetailsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
