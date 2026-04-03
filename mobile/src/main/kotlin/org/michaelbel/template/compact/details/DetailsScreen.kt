9955d5f2fe754b6bfb957655dcb0bf240680dff1
michaelbel <michael-bel@outlook.com>
2026-04-03 18:36:35 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/compact/details/DetailsScreen.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/compact/details/DetailsScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
