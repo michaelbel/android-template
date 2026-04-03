4aa3c0b2b9f6f224b422f3dbf2d141668a10543b
michaelbel <michael-bel@outlook.com>
2026-04-03 18:53:48 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/medium/main/about/AboutScreen.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/medium/main/about/AboutScreen.kt
@@ -66,7 +66,12 @@ fun AboutScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize(),
-            contentPadding = PaddingValues(start = 16.dp, top = 16.dp, end = 16.dp, bottom = if (isNavigationRail) navBarBottom else 16.dp)
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = if (isNavigationRail) navBarBottom else 16.dp
+            )
         ) {
             item {
                 ListItem(
