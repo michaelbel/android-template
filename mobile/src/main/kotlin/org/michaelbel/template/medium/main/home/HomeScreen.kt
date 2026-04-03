3422a34c2ca9b295dabce9e3bf2f904fcd9b8c4e
michaelbel <michael-bel@outlook.com>
2026-04-03 18:23:51 +0300

    Update project

---
+++ mobile/src/main/kotlin/org/michaelbel/template/medium/main/home/HomeScreen.kt
@@ -0,0 +1,82 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package org.michaelbel.template.medium.main.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.koinViewModel
+import org.michaelbel.template.ui.list.ListViewModel
+import org.michaelbel.template.ui.list.ui.BoarCard
+
+@Composable
+fun HomeScreen(
+    isNavigationRail: Boolean,
+    onNavigateToDetails: (Int) -> Unit,
+    viewModel: ListViewModel = koinViewModel()
+) {
+    val state by viewModel.stateFlow.collectAsStateWithLifecycle()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val navBarBottom = if (isNavigationRail) WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() else 0.dp
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Medium"
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        },
+        contentWindowInsets = if (isNavigationRail) WindowInsets(0, 0, 0, 0) else ScaffoldDefaults.contentWindowInsets
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = if (isNavigationRail) navBarBottom else 16.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = state.entities
+            ) { entity ->
+                BoarCard(
+                    entity = entity,
+                    onClick = onNavigateToDetails
+                )
+            }
+        }
+    }
+}
