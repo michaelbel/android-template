0cd98383245a1ce9f03ff1965484880f98bfe83c
michaelbel <michael-bel@outlook.com>
2026-04-03 18:53:39 +0300

    Update project

---
+++ mobile/src/main/kotlin/org/michaelbel/template/medium/details/DetailsScreen.kt
@@ -0,0 +1,120 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package org.michaelbel.template.medium.details
+
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
+import org.michaelbel.shared.middleLargeIncreasedListItemShape
+import org.michaelbel.template.AppRoute
+import org.michaelbel.template.ui.details.DetailsViewModel
+
+@Composable
+fun DetailsScreen(
+    route: AppRoute.Details,
+    viewModel: DetailsViewModel = koinViewModel { parametersOf(route) }
+) {
+    val state by viewModel.stateFlow.collectAsStateWithLifecycle()
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val navBarBottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = state.entity.name,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { backDispatcher?.onBackPressed() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        },
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = navBarBottom
+            )
+        ) {
+            item {
+                AsyncImage(
+                    model = state.entity.picture,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16F / 9F)
+                        .clip(middleLargeIncreasedListItemShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            item {
+                Spacer(
+                    modifier = Modifier.height(16.dp)
+                )
+            }
+            item {
+                Text(
+                    text = state.entity.description,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 18.sp)
+                )
+            }
+        }
+    }
+}
