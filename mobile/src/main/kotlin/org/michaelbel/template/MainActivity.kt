2882aede6d7f4ee727f97da5dd9c4b4dc3f69870
michaelbel <michael-bel@outlook.com>
2026-04-13 10:45:02 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/MainActivity.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/MainActivity.kt
@@ -8,12 +8,6 @@ import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
-import org.michaelbel.template.step1_Scaffold_BottomBar.Step1App
-import org.michaelbel.template.step2_NavigationSuiteScaffold_BottomBar.Step2App
-import org.michaelbel.template.step3_NavigationSuiteScaffold_BottomBar.Step3App
-import org.michaelbel.template.step4_NavigationSuiteScaffold_NavigationRail.Step4App
-import org.michaelbel.template.step5_NavigationSuiteScaffold_NavigationRail.Step5App
-import org.michaelbel.template.step6_NavigationSuiteScaffold_NavigationRailExpanded.Step6App
 import org.michaelbel.template.ui.AppTheme
 
 class MainActivity: ComponentActivity() {
@@ -28,15 +22,7 @@ class MainActivity: ComponentActivity() {
             AppTheme(
                 dynamicColors = state.dynamicColorsEnabled
             ) {
-                val index = 5
-                when (index) {
-                    0 -> Step1App()
-                    1 -> Step2App()
-                    2 -> Step3App()
-                    3 -> Step4App()
-                    4 -> Step5App()
-                    5 -> Step6App()
-                }
+                MainActivityContent()
             }
         }
     }
