0f94496b74ed07c24f1f0436466807d380551858
michaelbel <michael-bel@outlook.com>
2026-04-13 10:50:42 +0300

    Update project

--- mobile/src/main/kotlin/org/michaelbel/template/ui/about/AboutScreen.kt
+++ mobile/src/main/kotlin/org/michaelbel/template/ui/about/AboutScreen.kt
@@ -3,30 +3,30 @@
 package org.michaelbel.template.ui.about
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import org.michaelbel.shared.bottomListItemShape
 import org.michaelbel.shared.icons.Github
@@ -35,88 +35,63 @@ import org.michaelbel.shared.topListItemShape
 
 @Composable
 fun AboutScreen() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val uriHandler = LocalUriHandler.current
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
-                title = {
-                    Text(
-                        text = "About"
-                    )
-                }
+                title = { Text(text = "About") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
+                scrollBehavior = scrollBehavior
             )
-        }
+        },
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.only(WindowInsetsSides.Horizontal)
     ) { innerPadding ->
-        val layoutDirection = LocalLayoutDirection.current
-
         LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(
-                start = innerPadding.calculateStartPadding(layoutDirection),
-                top = innerPadding.calculateTopPadding().plus(16.dp),
-                end = innerPadding.calculateEndPadding(layoutDirection),
-                bottom = innerPadding.calculateBottomPadding().plus(16.dp)
-            )
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
         ) {
             item {
                 ListItem(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
                         .clip(topListItemShape)
-                        .clickable {},
-                    colors = ListItemDefaults.colors(containerColor = MaterialTheme.colorScheme.inversePrimary),
-                    headlineContent = {
-                        Text(
-                            text = "GitHub",
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                    },
-                    supportingContent = {
-                        Text(
-                            text = "View the Repository",
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    },
+                        .clickable { uriHandler.openUri("https://github.com/michaelbel") },
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerHighest
+                    ),
+                    headlineContent = { Text(text = "Star on GitHub") },
                     leadingContent = {
                         Icon(
                             imageVector = Github,
-                            contentDescription = null,
-                            modifier = Modifier.size(IconButtonDefaults.smallIconSize)
+                            contentDescription = null
                         )
                     }
-                )
-            }
-            item {
-                Spacer(
-                    modifier = Modifier.height(2.dp)
                 )
             }
             item {
                 ListItem(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
                         .clip(bottomListItemShape)
-                        .clickable {},
-                    colors = ListItemDefaults.colors(containerColor = MaterialTheme.colorScheme.inversePrimary),
-                    headlineContent = {
-                        Text(
-                            text = "Telegram",
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                    },
-                    supportingContent = {
-                        Text(
-                            text = "Subscribe to Channel",
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    },
+                        .clickable { uriHandler.openUri("https://t.me/android_career") },
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerHighest
+                    ),
+                    headlineContent = { Text(text = "Telegram Channel") },
                     leadingContent = {
                         Icon(
                             imageVector = Telegram,
-                            contentDescription = null,
-                            modifier = Modifier.size(IconButtonDefaults.smallIconSize)
+                            contentDescription = null
                         )
                     }
                 )
